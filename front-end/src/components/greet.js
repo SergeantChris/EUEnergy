7f10f1f7c5e92ad899ec0660b58136d2102f7127
George Tsagkarelis <athina.terzoglou@gmail.com>
2020-02-21 20:31:38 +0200

    Add basic layout for app

---
+++ front-end/src/components/greet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//function Greet() {
+//	return <h3>Hello there! Im gREEting you!</h3>;
+//}
+
+export const Greet = () => <h3>Wankafankamon</h3>
+
+export const Greet2 = () => <h3>I think it works</h3>
+
+
+
+

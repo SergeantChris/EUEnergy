235a5f3e01cf2f8c63741e2eba4ec6c711b395e4
SergeantChris <christina.sartzetaki@gmail.com>
2020-02-29 13:24:28 +0200

    Implement HealthCheck Resource

--- back-end/src/main/java/restlet_api/resources/HealthCheckResource.java
+++ back-end/src/main/java/restlet_api/resources/HealthCheckResource.java
@@ -1,5 +1,22 @@
 package restlet_api.resources;
+import org.restlet.resource.Get;
+
+import com.mongodb.BasicDBObject;
+
+import restlet_api.databaseLayer.DatabaseManager;
+import restlet_api.utilities.GeneralUtilities;
 
 public class HealthCheckResource extends PowerResource{
-
+	
+	@Get
+	public String getCheck() {
+		String res = "";
+		DatabaseManager man = DatabaseManager.getManager();
+		if(man.getItem("Users", new BasicDBObject()).isEmpty()) {
+			res =  GeneralUtilities.STATUS_BAD_REQUEST;
+		}
+		else
+			res = GeneralUtilities.STATUS_OK;
+		return res;
+	}
 }

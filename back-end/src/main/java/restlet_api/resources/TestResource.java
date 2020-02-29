be90b28174cb19745987be8eea5dde4d2370b4b8
George Tsagkarelis <georgiofiorenchi@gmail.com>
2020-02-29 20:37:19 +0200

    Complete implementation of CSV upload on test resource

--- back-end/src/main/java/restlet_api/resources/TestResource.java
+++ back-end/src/main/java/restlet_api/resources/TestResource.java
@@ -1,6 +1,8 @@
 package restlet_api.resources;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 
 import org.apache.commons.fileupload.FileItemIterator;
@@ -15,11 +17,12 @@ import org.restlet.resource.Post;
 import org.restlet.resource.Status;
 
 import restlet_api.databaseLayer.DatabaseManager;
+import restlet_api.utilities.GeneralUtilities;
 
 public class TestResource extends PowerResource{
 	@Post
-	public Representation accept(Representation entity) throws Exception {
-	    Representation result = null;
+	public String accept(Representation entity) throws Exception {
+	    String result = null;
 	    if (entity != null) {
 	        if (MediaType.MULTIPART_FORM_DATA.equals(entity.getMediaType(), true)) {
 	            // 1/ Create a factory for disk-based file items
@@ -42,20 +45,35 @@ public class TestResource extends PowerResource{
 	                FileItemStream fi = fileIterator.next();
 	                if (fi.getFieldName().equals("fileToUpload")) {
 	                    found = true;
+	                    File theFile = new File("theFile.csv");
+	                    FileWriter writer  = new FileWriter("theFile.csv");
 	                    // consume the stream immediately, otherwise the stream
 	                    // will be closed.
-	                    StringBuilder sb = new StringBuilder("media type: ");
+	                    
+	                    StringBuilder sb = new StringBuilder();
+	                    /*
 	                    sb.append(fi.getContentType()).append("\n");
 	                    sb.append("file name : ");
 	                    sb.append(fi.getName()).append("\n");
+	                    */
 	                    BufferedReader br = new BufferedReader(
 	                            new InputStreamReader(fi.openStream()));
 	                    String line = null;
 	                    while ((line = br.readLine()) != null) {
 	                        sb.append(line);
+	                        sb.append('\n');
 	                    }
 	                    sb.append("\n");
-	                    result = new StringRepresentation(sb.toString(), MediaType.TEXT_PLAIN);
+	                    System.out.println(sb.toString());
+	                    writer.write(sb.toString());
+	                    writer.close();
+	                    
+	                    GeneralUtilities.executeSystemCommand("./mongoInsertCSV theFile.csv automateTest");
+	                    
+	                    theFile.delete();
+	                    
+	                    
+	                    result = GeneralUtilities.STATUS_OK + '\n';
 	                }
 	            }
 	        } else {

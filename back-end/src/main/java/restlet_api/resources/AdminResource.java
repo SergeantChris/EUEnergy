3cfa398d3d97b6b308d527639bee8a19ec01d79e
SergeantChris <christina.sartzetaki@gmail.com>
2020-02-29 02:07:29 +0200

    Admin Put/Get method implementation, addition of other required fields

--- back-end/src/main/java/restlet_api/resources/AdminResource.java
+++ back-end/src/main/java/restlet_api/resources/AdminResource.java
@@ -1,6 +1,9 @@
 package restlet_api.resources;
 
+import org.json.JSONObject;
+import org.restlet.resource.Get;
 import org.restlet.resource.Post;
+import org.restlet.resource.Put;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoWriteException;
@@ -20,6 +23,7 @@ public class AdminResource extends PowerResource{
 				case "users":
 					res = createUser();
 					break;
+				// case "Atl|Agpt|Datlf"
 				}
 			}
 			else
@@ -30,21 +34,107 @@ public class AdminResource extends PowerResource{
 		return res;
 	}
 	
+	@Put
+	public String getPut() {
+		String action = getMandatoryAttribute("Action", "Action");
+		String res = "";
+		String token = getRequest().getHeaders().getFirstValue("Token");
+		if(DatabaseManager.isActiveToken(token)) {
+			if(DatabaseManager.getUsernameFromToken(token).equals("admin")) {
+				res = updateUser(action);
+			}
+			else
+				res = GeneralUtilities.STATUS_NOT_AUTHORIZED;
+		}
+		else
+			res = GeneralUtilities.STATUS_NOT_AUTHORIZED;
+		return res;
+	}
+	
+	@Get
+	public String getGet() {
+		String action = getMandatoryAttribute("Action", "Action");
+		String res = "";
+		String token = getRequest().getHeaders().getFirstValue("Token");
+		if(DatabaseManager.isActiveToken(token)) {
+			if(DatabaseManager.getUsernameFromToken(token).equals("admin")) {
+				res = showUser(action);
+			}
+			else
+				res = GeneralUtilities.STATUS_NOT_AUTHORIZED;
+		}
+		else
+			res = GeneralUtilities.STATUS_NOT_AUTHORIZED;
+		return res;
+	}
+	
 	public String createUser() {
 		String res = "";
 		try {
 			String username = getRequest().getHeaders().getFirstValue("User");
 			String pass = getRequest().getHeaders().getFirstValue("Pass");
+			String email = getRequest().getHeaders().getFirstValue("Email");
+			String quotas = getRequest().getHeaders().getFirstValue("Quotas");
 			int hashpass = pass.hashCode();
-			BasicDBObject dbo = new BasicDBObject();
 			
+			BasicDBObject dbo = new BasicDBObject();
 			dbo.append("User", username);
 			dbo.append("Pass", hashpass);
-			dbo.append("Quotas", 12);
+			dbo.append("Email", email);
+			dbo.append("Quotas", Integer.parseInt(quotas));
 			System.out.println("Dbo is " + dbo.toString());
 			try {
 				DatabaseManager.getManager().addItem("Users", dbo);
 				res = GeneralUtilities.STATUS_OK;
+				System.out.println("Res is OK");
+			}catch(MongoWriteException e) {
+				res =  GeneralUtilities.STATUS_BAD_REQUEST;
+				System.out.println("Res is BAD");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	public String updateUser(String username) {
+		String res = "";
+		try {
+			String pass = getRequest().getHeaders().getFirstValue("Pass");
+			String email = getRequest().getHeaders().getFirstValue("Email");
+			String quotas = getRequest().getHeaders().getFirstValue("Quotas");
+			int hashpass = pass.hashCode();
+
+			BasicDBObject filter = new BasicDBObject();
+			BasicDBObject dbo = new BasicDBObject();
+			filter.append("User", username);
+			dbo.append("Pass", hashpass);
+			dbo.append("Email", email);
+			dbo.append("Quotas", Integer.parseInt(quotas));
+			System.out.println("Dbo is " + dbo.toString());
+			try {
+				DatabaseManager.getManager().updItem("Users", filter, dbo);
+				res = GeneralUtilities.STATUS_OK;
+				System.out.println("Res is OK");
+			}catch(MongoWriteException e) {
+				res =  GeneralUtilities.STATUS_BAD_REQUEST;
+				System.out.println("Res is BAD");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	public String showUser(String username) {
+		String res = "";
+		try {
+			BasicDBObject filter = new BasicDBObject();
+			filter.append("User", username);
+			try {
+				BasicDBObject doc =  DatabaseManager.getManager().getItem("Users", filter);
+				String token = DatabaseManager.getTokenFromUsername(username);
+				res = GeneralUtilities.STATUS_OK + doc.toString() + token;
 				System.out.println("Res is OK");
 			}catch(MongoWriteException e) {
 				res =  GeneralUtilities.STATUS_BAD_REQUEST;

69ecba95a902eff01c1a9c0919f194b2fecdec83
kwpap <kwpaptypetomaniac97@gmail.com>
2020-02-29 16:50:06 +0200

    Area file addition

--- front-end/src/components/Dropdown.js
+++ front-end/src/components/Dropdown.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Select from 'react-select';
-import test from './test.json'
+import Area from './Area.json'
+import Type from './Type.json'
+
 
 
 const Dropdown = (props) => {
-    console.log(test)
+    console.log(Type)
     return <div style = {selectStyle}>
         <Select
           className="basic-single"
           classNamePrefix="select"
-          defaultValue={test[0]}
+          defaultValue={Type[0]}
           name="type"
-          options={test}
+          options={Type}
         />
+        This could have been a nice place to import information about the above selection for example. I am not sure wet, but we could definately do it. Also, this test sould be updated in case of emergency. Just kidding. It sould be updated on falase selection. It could also contain a definition of the above selections.
+
     </div>
 }
 
@@ -21,5 +25,6 @@ export default Dropdown
 const selectStyle = {
     width:500,
     paddingRight: 30,
-    paddingLeft: 30
+    paddingLeft: 30,
+    height: 300
 }

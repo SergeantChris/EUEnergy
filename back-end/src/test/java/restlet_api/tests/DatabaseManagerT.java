3612a54454ce2d70a2d69c816fa56a13df45e285
SergeantChris <christina.sartzetaki@gmail.com>
2020-03-02 14:57:38 +0200

    JUnit tests setup

---
+++ back-end/src/test/java/restlet_api/tests/DatabaseManagerT.java
@@ -0,0 +1,13 @@
+package restlet_api.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DatabaseManagerT {
+	@Test
+	public void testAdd() {
+		System.out.println("This test method should be run");
+	    assertEquals(42, Integer.sum(19, 23));
+	}
+}
---
+++ back-end/src/test/java/restlet_api/tests/TestTest.java
@@ -0,0 +1,11 @@
+package restlet_api.tests;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+	@Test
+	public void testAdd() {
+		System.out.println("This test method should be run");
+	    assertEquals(42, Integer.sum(19, 23));
+	}
+}
